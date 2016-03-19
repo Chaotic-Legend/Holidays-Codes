b81637b1e9437d6488f54f43048c5646eb148399
Isaac Hoyos <gamemasterx40@gmail.com>
2016-03-19 11:45:21 -0400

    Added files via upload

---
+++ Easter.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <cs50.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main(void)
+
+{
+    printf("The Easter Bunny ate all of the carrots we left for him. What a pig.-Steve Carell\n");
+    printf("Happy Easter!\n");
+    printf("From Isaac Hoyos!\n");
+}
---
+++ Saint_Patricks_Day.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <cs50.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main(void)
+
+{
+    printf("St. Patrick's Day is an enchanted time - a day to begin transforming winter's dreams into summer's magic.~Adrienne Cook\n");
+    printf("Happy St. Patrick's Day!\n");
+    printf("From Isaac Hoyos!\n");
+    
+}
