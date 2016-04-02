947f1192aaa0a749755cbcaf8e4f43cce1df21b6
Isaac Hoyos <gamemasterx40@gmail.com>
2016-04-01 20:04:20 -0400

    Added files via upload

---
+++ April_Fools_Day.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <cs50.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main(void)
+
+{
+    printf("The fool doth think he is wise, but the wise man knows himself a fool. — William Shakespeare\n");
+    printf("Happy April Fools Day!\n");
+    printf("From Isaac Hoyos!\n");
+}
